4639ac249eb19a70250cac2eafc5f907085953f2
Atilla <oomenatilla@gmail.com>
2023-05-25 10:02:38 +0200

    Nieuwe file, bijna klaar

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
-let JSONdata = [];
+let monster = [];
 
 window.addEventListener("load", function () {
   fetch(
     "https://dl.dropboxusercontent.com/s/iwz112i0bxp2n4a/5e-SRD-Monsters.json"
   )
-    .then(klaarMetLaden)
+    .then((response) => response.json())
     .then(laadData);
 });
 
-function klaarMetLaden(response) {
-  return response.json();
+function laadData(data) {
+  monster = data;
+  console.log(monster);
+  for (let i = 0; i < 10; i++) {
+    showMonster(monster[i]);
+  }
 }
 
-function laadData(data) {
-  JSONdata = data;
-  console.log(JSONdata);
+function showMonster(monster) {
+  const monsterTemplate = document.getElementById("monster-template");
+  const monsterArticle = monsterTemplate.content.cloneNode(true);
+  monsterArticle.querySelector("h1.name").innerHTML = monster.name;
+  monsterArticle.querySelector(
+    "p.description"
+  ).innerHTML = `${monster.size} ${monster.alignment} ${monster.type}`;
+
+  document.body.appendChild(monsterArticle);
 }
